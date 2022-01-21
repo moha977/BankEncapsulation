b331b9db86e238e0bac39192e0507676902ed80c
Mohammad ahma <ahmad.moh0812@icloud.com>
2022-01-21 12:26:21 -0500

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+   public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+
+       public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! your balance is now {account.GetBalance()}");
+        }
+    }
+}
